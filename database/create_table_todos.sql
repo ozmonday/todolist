1877b2b203a5bf2d9f18442486e7329be83ea7e1
HadiOZ <hadioz616@gmail.com>
2022-11-13 07:58:39 +0700

    almost finish

--- database/create_table_todos.sql
+++ database/create_table_todos.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS todos (
   id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  title varchar(255) NOT NULL,
+  title varchar(255) NOT NULL DEFAULT 'unknow',
   activity_group_id int NOT NULL,
   is_active bool NOT NULL DEFAULT true,
   priority varchar(55) NOT NULL DEFAULT 'very-high',
